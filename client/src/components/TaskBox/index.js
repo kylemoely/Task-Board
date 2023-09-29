d1e3db591eeffb2bd8c6057ba46acc7617075ca6
kylemoely <kylemoely@gmail.com>
2023-09-29 12:55:51 -0600

    Made TaskBox properly render all information

--- client/src/components/TaskBox/index.js
+++ client/src/components/TaskBox/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css'
+import Initials from '../Initials';
 
-export default function TaskBox() {
+export default function TaskBox({ task }) {
+    const firstUser = task.users[0];
     return(
         <div className='bordered rounded w-100 m-1 d-flex flex-column' draggable='true'>
             <div className='assignees d-flex justify-content-end'>
-                <div className='assignee bg-danger m-1'>KM</div>
-                <div className='assignee bg-danger m-1'>KM</div>
+                {task.users.length>1 ? <div><Initials size={14} color={firstUser.color} firstName={firstUser.firstName} lastName={firstUser.lastName}/>+{task.users.length-1}</div> : task.users.length===1 ? <Initials size={14} color={firstUser.color} firstName={firstUser.firstName} lastName={firstUser.lastName}/> : <></>}
             </div>
-            <div className='task m-1'>Write a message to someone about anythign you want</div>
+            <div className='fw-bold m-1 fs-4'>{task.title}</div>
+            <div className='task m-1'>{task.description}</div>
         </div>
     )
 }
--- client/src/components/TaskList/index.js
+++ client/src/components/TaskList/index.js
@@ -7,7 +7,7 @@ export default function Tasklist(props) {
     return(
         <div className='bordered rounded col-md-3 h-100 d-flex align-items-center flex-column'>
             <div className='h4 text-center'>{props.status}</div>
-            {props.tasks.map(task => <TaskBox task={task} />)}
+            {props.tasks.map((task, i) => <TaskBox key={i} task={task} />)}
         </div>
     )
 }
