b82a35adb64431eb7e6d9e87965e494aac514ed7
kylemoely <kylemoely@gmail.com>
2023-10-06 10:13:34 -0600

    Fixed NewTask button styling. Added invite button

--- client/src/pages/Project/index.js
+++ client/src/pages/Project/index.js
@@ -9,6 +9,7 @@ import useAuth from '../../hooks/useAuth';
 import { useState, useEffect } from 'react';
 import useAxiosPrivate from '../../hooks/useAxiosPrivate';
 import CreateTask from '../../components/CreateTask';
+import Invite from '../../components/Invite';
 
 export default function Project() {
 
@@ -43,8 +44,11 @@ export default function Project() {
             <Row className='h-100 justify-content-between'>
                 <Sidebar />
                 <section className='col-md-9 d-flex flex-column'>
-                    <div className='projectTitle h2 text-center'>{projectData.title} <CreateTask setReload={setReload} users={projectData.users} projectId={params.projectId}/></div>
-                    
+                    <div className='projectTitle h2 text-center'>{projectData.title}</div>
+                    <div className='m-1 d-flex justify-content-between'>
+                        <CreateTask setReload={setReload} users={projectData.users} projectId={params.projectId}/>
+                        <Invite />
+                    </div>
                     <Row className='h-100 d-flex justify-content-around'>
                         <TaskList setReload={setReload} status='To Do'tasks={projectData.toDoTasks}/>
                         <TaskList setReload={setReload} status='Doing' tasks={projectData.doingTasks}/>
