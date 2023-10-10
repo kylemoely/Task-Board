5c400221856ebbdf98c5ccf9bc14aace139a8af2
kylemoely <kylemoely@gmail.com>
2023-10-10 13:39:13 -0600

    fixed sequelize associations

--- server/models/index.js
+++ server/models/index.js
@@ -2,19 +2,46 @@ const User = require('./User');
 const Project = require('./Project');
 const Task = require('./Task');
 const Notification = require('./Notification');
+const sequelize = require('../config/connection');
+const { DataTypes } = require('sequelize');
 
 Project.hasMany(Task, {
     foreignKey: 'projectId'
 });
 Task.belongsTo(Project);
 
-User.belongsToMany(Project, { through: 'UserProjects' });
-Project.belongsToMany(User, { through: 'UserProjects' });
+const UserProjects = sequelize.define('UserProjects', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false }},
+        { timestamps: false });
 
-User.belongsToMany(Task, { through: 'UserTasks' });
-Task.belongsToMany(User, { through: 'UserTasks' });
+User.belongsToMany(Project, { through: UserProjects });
+Project.belongsToMany(User, { through: UserProjects });
 
-User.belongsToMany(Notification, { through: 'UserNotifications' });
-Notification.belongsToMany(User, { through: 'UserNotifications' });
+const UserTasks = sequelize.define('UserTasks', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false }},
+        { timestamps: false });
+
+
+User.belongsToMany(Task, { through: UserTasks });
+Task.belongsToMany(User, { through: UserTasks });
+
+const UserNotifications = sequelize.define('UserNotifications', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false }},
+        { timestamps: false });
+
+User.belongsToMany(Notification, { through: UserNotifications });
+Notification.belongsToMany(User, { through: UserNotifications });
 
 module.exports = { User, Project, Task, Notification };
