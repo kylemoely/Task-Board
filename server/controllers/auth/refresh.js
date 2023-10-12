befbe6aad2066c744513486b2fc427913e985c4e
kylemoely <kylemoely@gmail.com>
2023-10-12 11:18:39 -0600

    Changed some error status codes

--- server/controllers/auth/refresh.js
+++ server/controllers/auth/refresh.js
@@ -10,10 +10,10 @@ const handleRefreshToken = async (req, res) => {
         where: { refreshToken },
         include: [Project]
     });
-    if(!checkUser) return res.sendStatus(403);
+    if(!checkUser) return res.sendStatus(401);
 
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
-        if(err || checkUser.email !== decoded.email) return res.sendStatus(403);
+        if(err || checkUser.email !== decoded.email) return res.sendStatus(401);
         const user = { 
             id: checkUser.id,
             email: checkUser.email,
