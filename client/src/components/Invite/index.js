67c3042e56199fcf326b442cfdaf0fc3140d4ea4
kylemoely <kylemoely@gmail.com>
2023-10-13 13:20:10 -0600

    Added error message to invite component

--- client/src/components/Invite/index.js
+++ client/src/components/Invite/index.js
@@ -7,23 +7,32 @@ export default function Invite (props) {
     const axiosPrivate = useAxiosPrivate();
     const [show, setShow] = useState(false);
     const [email, setEmail] = useState('');
+    const [userFound, setUserFound] = useState(true);
 
     const handleClose = () => {
         setEmail('');
         setShow(false);
+        setUserFound(true);
     }
     const handleShow = () => setShow(true);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const putResponse = await axiosPrivate.put(`/api/projects/${props.projectId}/add`, JSON.stringify({ email }));
-        await axiosPrivate.post('/api/notifications', JSON.stringify({
-            type: 'invitedYou',
-            str2: props.project,
-            link: `/project/${props.projectId}`,
-            recipients: [putResponse.data[1]]
-        }));
-        handleClose();
+        try{
+            const putResponse = await axiosPrivate.put(`/api/projects/${props.projectId}/add`, JSON.stringify({ email }));
+            await axiosPrivate.post('/api/notifications', JSON.stringify({
+                type: 'invitedYou',
+                str2: props.project,
+                link: `/project/${props.projectId}`,
+                recipients: [putResponse.data[1]]
+            }));
+            handleClose();
+        } catch(err){
+            setUserFound(false);
+            console.log(err);
+        }
+        
+        
     }
 
     return (
@@ -49,6 +58,7 @@ export default function Invite (props) {
                                 onChange={(e) => setEmail(e.target.value)}
                                 autoFocus
                             />
+                            <p className={userFound ? 'offscreen' : 'h6 text-danger'}>User not found.</p>
                         </Form.Group>
                     </Form>
                 </Modal.Body>
