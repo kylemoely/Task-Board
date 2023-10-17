e3f8dd59546335102916c091fb5bcf55ad3f8be1
kylemoely <kylemoely@gmail.com>
2023-10-17 13:01:00 -0600

    Error message was not setting properly. Fixed it

--- client/src/hooks/handleError.js
+++ client/src/hooks/handleError.js
@@ -1,15 +1,13 @@
-import { useState } from 'react';
-
 const handleError = (err, setErr) => {
-    switch(err.response.status[0]){
-        case 4:
+    switch(err.response.status.toString()[0]){
+        case '4':
             setErr(`Something wasn't found. Please try again.`);
             break;
-        case 5:
+        case '5':
             setErr(`Something went wrong on our end. Please try again later.`);
             break;
         default:
-            setErr(`Something went wrong. Please try again`);
+            setErr('Something went wrong. Please try again.');
     }
 }
 
