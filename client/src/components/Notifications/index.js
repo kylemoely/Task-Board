2a859e1fa3f9260d887f5254a109b3257191d439
kylemoely <kylemoely@gmail.com>
2023-10-17 13:14:11 -0600

    Actually added scroll functionality to dashboard notifications

--- client/src/components/Notifications/index.js
+++ client/src/components/Notifications/index.js
@@ -11,12 +11,12 @@ export default function Notifications({ notifications }) {
     return(
             <Row style={{backgroundColor: 'white', boxShadow: '0px 2px 4px 2px rgb(138, 138, 138)'}} className='bordered h-50 rounded d-flex flex-column'>
                 <div className='h2'>Notifications</div>
-                <div className='h-75 overflow-scroll mb-1'>
+                <div className='h-75 notArea mb-1'>
                     {notifications.map((not, i) => {
                         return <Notification key={i} notification={not}/>
                     })}
                 </div>
-                <div className='button w-25 align-self-center text-center'>View All</div>
+                <div style={ notifications.length>0 ? {display: 'block'} : { display: 'none' }} className='button w-25 align-self-center text-center'>View All</div>
             </Row>
     );
 }
